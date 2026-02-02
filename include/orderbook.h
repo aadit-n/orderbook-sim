e22fcb6595f7a840b007787475c46884fb6106ee
Aadit Nair <81066624+aadit-n@users.noreply.github.com>
2026-02-02 15:31:22 +0530

    removed print order book method

--- include/orderbook.h
+++ include/orderbook.h
@@ -10,17 +10,6 @@ class OrderBook{
         vector<order> buy;
         vector<order> sell;
         vector<order> fulfilled; 
-        
-    void printOrderBook(){
-        std::cout << "BID:\n";
-        for (int i = 0; i < buy.size(); i++){
-            buy[i].printOrder();
-        }
-        std::cout << "ASK:\n";
-        for (int i = 0; i < sell.size(); i++){
-            sell[i].printOrder();
-        }
-    }
 };
 void addOrder(OrderBook &book, order &newOrder);
 void orderExpiry(OrderBook &book);
